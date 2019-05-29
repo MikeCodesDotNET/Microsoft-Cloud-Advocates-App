f1222931934c15f1296f8dbacd4934ae86ddc879
Mike James <mike@xamarin.com>
2019-05-29 14:05:08 +0100

    Initial Commit

---
+++ Advocates.iOS/Renderers/CleanNavigationBarRenderer.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Advocates.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(Page), typeof(CleanNavigationBarRenderer))]
+namespace Advocates.iOS.Renderers
+{
+    public class CleanNavigationBarRenderer : PageRenderer
+    {
+        App formsApp = new App();
+
+        protected override void OnElementChanged(VisualElementChangedEventArgs e)
+        {
+            base.OnElementChanged(e);
+
+            UINavigationBar.Appearance.SetTitleTextAttributes(new UITextAttributes
+            {
+                Font = UIFont.FromName("Poppins SemiBold", 20),
+                TextColor = ((Color)formsApp.Resources["AccentColor"]).ToUIColor()
+            });
+
+            // Remove transparency, shadow and separator line
+            UINavigationBar.Appearance.Translucent = false;
+            UINavigationBar.Appearance.ShadowImage = new UIImage();
+            UINavigationBar.Appearance.SetBackgroundImage(new UIImage(), UIBarMetrics.Default);
+
+            UINavigationBar.Appearance.BackgroundColor = Color.White.ToUIColor();
+            UINavigationBar.Appearance.BarTintColor = Color.White.ToUIColor();
+            UINavigationBar.Appearance.TintColor = ((Color)formsApp.Resources["AccentColor"]).ToUIColor();
+
+            UINavigationBar.Appearance.LargeTitleTextAttributes = new UIStringAttributes
+            {
+                ForegroundColor = ((Color)formsApp.Resources["AccentColor"]).ToUIColor(),
+                Font = UIFont.FromName("Poppins SemiBold", 34),
+            };
+
+            UIBarButtonItem.Appearance.SetTitleTextAttributes(new UITextAttributes
+            {
+                Font = UIFont.FromName("Poppins SemiBold", 14),
+                TextColor = ((Color)formsApp.Resources["AccentColor"]).ToUIColor()
+            }, UIControlState.Normal);
+        }
+    }
+}
---
+++ Advocates.iOS/Renderers/CleanTabbedRenderer.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Advocates.iOS.Renderers;
+using UIKit;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.iOS;
+
+[assembly: ExportRenderer(typeof(TabbedPage), typeof(CleanTabbedRenderer))]
+namespace Advocates.iOS.Renderers
+{
+    public class CleanTabbedRenderer : TabbedRenderer
+    {
+        App formsApp = new App();
+
+        public CleanTabbedRenderer()
+        {
+            TabBar.Translucent = false;
+            //TabBar.Opaque = true;
+
+            // Remove shadow and separator line
+            TabBar.BackgroundImage = new UIImage();
+            TabBar.ShadowImage = new UIImage();
+
+            UITabBar.Appearance.BackgroundColor = ((Color)formsApp.Resources["AccentColor"]).ToUIColor();
+            UITabBarItem.Appearance.SetTitleTextAttributes(new UITextAttributes() { Font = UIFont.FromName("Avenir-Medium", 12), TextColor = UIColor.White }, UIControlState.Normal);
+        }
+
+    }
+}
